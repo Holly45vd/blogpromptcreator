5c46b743b3c45d56dc30c0a8a32a46b057a56c37
Holly45vd <99254184+Holly45vd@users.noreply.github.com>
2026-05-27 08:38:05 +0000

    fix: App 컴포넌트에 BrowserRouter basename 속성 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import PromptResultPage from "./pages/PromptResultPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    // basename 속성을 주입하여 GitHub Pages의 하위 경로(/blogpromptcreator/)를 인식하게 만듦
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<FormPage />} />
         <Route path="/result" element={<PromptResultPage />} />
